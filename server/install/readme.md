fc8c65edbcbb3872ebe00059fb2370dc4bf23eea
Jarda K <jaroslav.krch@gmail.com>
2025-07-26 06:55:10 +0200

    rozsireni pro logovani pro fastapi

--- server/install/readme.md
+++ server/install/readme.md
@@ -1,7 +1,21 @@
-# chmod +x register_fastapi.sh unregister_fastapi.sh
+# logy
+mkdir -p /data/logs/fastapi
+chown user:user /data/logs/fastapi
+chmod 755 /data/logs/fastapi
 
-# ./register_fastapi.sh
+touch /data/logs/fastapi/fastapi.log
+chmod 664 /data/logs/fastapi/fastapi.log
 
-# systemctl status fastapi-server
+# vypis
+nano /data/logs/fastapi/fastapi.log 
 
-# ./unregister_fastapi.sh 
+# installace
+chmod +x register_fastapi.sh unregister_fastapi.sh
+./register_fastapi.sh
+systemctl status fastapi-server
+
+# uninstallace
+./unregister_fastapi.sh 
+
+
+
--- server/install/register_fastapi.sh
+++ server/install/register_fastapi.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 SERVICE_PATH="/etc/systemd/system/fastapi-server.service"
+LOG_DIR="/robot/data/logs/fastapi"
+LOG_FILE="$LOG_DIR/fastapi.log"
+
+echo "Creating log directory..."
+mkdir -p $LOG_DIR
+touch $LOG_FILE
+chmod 664 $LOG_FILE
 
 echo "Creating systemd service..."
 
@@ -12,9 +19,11 @@ After=network.target
 [Service]
 User=user
 WorkingDirectory=/robot/opt/projects/robotour/server
-ExecStart=/robot/opt/projects/robotour/venv-robotour/bin/uvicorn main:app --host 0.0.0.0 --port 8080
+ExecStart=/bin/bash -c '/robot/opt/projects/robotour/venv-robotour/bin/uvicorn main:app --host 0.0.0.0 --port 8080 >> $LOG_FILE 2>&1'
 Restart=always
 RestartSec=3
+StandardOutput=append:$LOG_FILE
+StandardError=append:$LOG_FILE
 
 [Install]
 WantedBy=multi-user.target
