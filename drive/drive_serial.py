2b5db790c6b967a98ea6cd7ea151d8168ca2af44
Jarda K <jaroslav.krch@gmail.com>
2025-07-31 05:43:58 +0200

    drive fix error in name

--- drive/drive_serial.py
+++ drive/drive_serial.py
@@ -17,15 +17,24 @@ def open_serial():
             dsrdtr=False,   # nepoužívat DTR
             rtscts=False    # nepoužívat RTS/CTS
         )
-        ser.setDTR(False)  # explicitně vypni DTR
-        ser.setRTS(False)  # explicitně vypni RTS        
+        serial_conn.setDTR(False)  # explicitně vypni DTR
+        serial_conn.setRTS(False)  # explicitně vypni RTS        
         print(f"Serial otevřen na {SERIAL_PORT}")
+
+        # Po otevření pošli hlavičku (bajt 13)
+        serial_conn.write(bytes([13]))
+        print("Odeslána hlavička (13)")
+
     else:
         print("Serial už je otevřený.")
 
 def close_serial():
     global serial_conn
     if serial_conn and serial_conn.is_open:
+         # Před zavřením pošli (27)
+        serial_conn.write(bytes([27]))
+        print("Odeslán ukončovací bajt (27)")
+
         serial_conn.close()
         print("Serial uzavřen.")
     else:
