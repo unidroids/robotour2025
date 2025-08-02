caa441d458d85d45774e649181087f1e29026f81
Jaroslav Krch <jaroslav.krch@gmail.com>
2025-08-02 08:38:56 +0200

    camera uprava prijimaci testovaci strany zmenseni

--- camera/test/recv.py
+++ camera/test/recv.py
@@ -6,6 +6,13 @@ import numpy as np
 IP_LAPTOP = "0.0.0.0"
 PORT = 5000
 
+def resize_to_screen(frame, max_width=1280, max_height=720):
+    h, w = frame.shape[:2]
+    scale = min(max_width / w, max_height / h, 1.0)  # scale nikdy nezvětšuje
+    if scale < 1.0:
+        frame = cv2.resize(frame, (int(w * scale), int(h * scale)), interpolation=cv2.INTER_AREA)
+    return frame
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((IP_LAPTOP, PORT))
 s.listen(1)
@@ -37,6 +44,7 @@ while True:
 
     # Dekóduj obrázek a zobraz
     frame = cv2.imdecode(np.frombuffer(data, dtype=np.uint8), 1)
+    frame = resize_to_screen(frame, 1280, 720)
     cv2.imshow('Jetson Camera', frame)
     if cv2.waitKey(1) == 27:
         break
